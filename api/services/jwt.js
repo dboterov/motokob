787208bb4dba17543eb58219acf610708e54bb99
Daniel Botero <dbotero@matisses.co>
2017-10-22 19:06:31 -0500

    Mensaje de log al crear el token

--- api/services/jwt.js
+++ api/services/jwt.js
@@ -4,7 +4,8 @@ var jwt = require('jwt-simple');
 var moment = require('moment');
 var secret = '?}U^;N3XU_Yq7~Y{NdD]_*R=(x4"1>';
 
-exports.createToken = function(user) {
+exports.createToken = function (user) {
+  console.log('creating token for user: ', user);
   var payload = {
     sub: user._id,
     name: user.name,
