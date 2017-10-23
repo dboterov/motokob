48dcf79db631a7ae39136b2d816ec336b9ba8049
Daniel Botero <dbotero@matisses.co>
2017-10-22 19:11:01 -0500

    Correccion en campos personalizados del token

--- api/services/jwt.js
+++ api/services/jwt.js
@@ -5,14 +5,13 @@ var moment = require('moment');
 var secret = '?}U^;N3XU_Yq7~Y{NdD]_*R=(x4"1>';
 
 exports.createToken = function (user) {
-  console.log('creating token for user: ', user);
   var payload = {
     sub: user._id,
     name: user.name,
     surname: user.surname,
     email: user.email,
-    role: user.role,
-    companies: user.companies,
+    permissions: user.permissions,
+    username: user.username,
     iat: moment().unix(),
     exp: moment().add(12, 'hours').unix()
   };
